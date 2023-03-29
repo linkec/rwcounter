f692203308cda54254f09f1185c3e7b953825e69
Link <linkec@live.com>
2023-03-29 19:57:13 +0800

    fix: rwmutex

--- counter.go
+++ counter.go
@@ -10,8 +10,8 @@ import (
 type Counter struct {
 	pool          []map[string]*CounterItem
 	mutex         []sync.RWMutex
-	windowSize    int
 	currentWindow int
+	cfg           CounterCfg
 }
 
 type CounterItem struct {
@@ -32,54 +32,58 @@ func NewCounter(cfg CounterCfg) *Counter {
 	c := &Counter{
 		pool:          make([]map[string]*CounterItem, cfg.PoolSize),
 		mutex:         make([]sync.RWMutex, cfg.PoolSize),
-		windowSize:    cfg.Windows,
 		currentWindow: time.Now().Second() % cfg.Windows,
+		cfg:           cfg,
 	}
 
 	for i := 0; i < cfg.PoolSize; i++ {
 		c.pool[i] = make(map[string]*CounterItem)
 	}
 
-	go func() {
-		t := time.NewTicker(cfg.GCInterval)
-		for {
-			<-t.C
-			for i, m := range c.pool {
-				c.mutex[i].Lock()
-				for k, v := range m {
-					if time.Since(v.Last) > cfg.TTL {
-						delete(m, k)
-					}
-				}
-				c.mutex[i].Unlock()
-			}
-		}
-	}()
+	go c.gc()
+	go c.windowsCollect()
 
-	go func() {
-		t := time.NewTicker(time.Second)
-		for {
-			<-t.C
-			// currentWindow :=
-			c.currentWindow = time.Now().Second() % cfg.Windows
-			nextWindow := c.currentWindow + 1
-			if nextWindow >= cfg.Windows {
-				nextWindow = 0
-			}
-
-			for i, m := range c.pool {
-				c.mutex[i].RLock()
-				for k, v := range m {
-					atomic.StoreInt64(v.Count, atomic.LoadInt64(v.InternalCount))
-					windowCount := atomic.LoadInt64(v.windows[nextWindow])
-					c.decr(k, windowCount)
-					atomic.StoreInt64(v.windows[nextWindow], 0)
-				}
-				c.mutex[i].RUnlock()
-			}
-		}
-	}()
 	return c
+}
+
+func (c *Counter) gc() {
+	t := time.NewTicker(c.cfg.GCInterval)
+	for {
+		<-t.C
+		for i, m := range c.pool {
+			c.mutex[i].Lock()
+			for k, v := range m {
+				if time.Since(v.Last) > c.cfg.TTL {
+					delete(m, k)
+				}
+			}
+			c.mutex[i].Unlock()
+		}
+	}
+}
+
+func (c *Counter) windowsCollect() {
+	t := time.NewTicker(time.Second)
+	for {
+		<-t.C
+		// currentWindow :=
+		c.currentWindow = time.Now().Second() % c.cfg.Windows
+		nextWindow := c.currentWindow + 1
+		if nextWindow >= c.cfg.Windows {
+			nextWindow = 0
+		}
+
+		for i, m := range c.pool {
+			c.mutex[i].RLock()
+			for _, v := range m {
+				atomic.StoreInt64(v.Count, atomic.LoadInt64(v.InternalCount))
+				windowCount := atomic.LoadInt64(v.windows[nextWindow])
+				atomic.AddInt64(v.InternalCount, -windowCount)
+				atomic.StoreInt64(v.windows[nextWindow], 0)
+			}
+			c.mutex[i].RUnlock()
+		}
+	}
 }
 
 func (c *Counter) hash(key string) int {
@@ -107,7 +111,7 @@ func (c *Counter) Incr(key string, step int64) {
 			windows:       make(map[int]*int64),
 		}
 		c.pool[idx][key] = counter
-		for i := 0; i < c.windowSize; i++ {
+		for i := 0; i < c.cfg.Windows; i++ {
 			c.pool[idx][key].windows[i] = new(int64)
 		}
 		wCounter = counter.windows[c.currentWindow]
@@ -143,13 +147,4 @@ func (c *Counter) Keys() []string {
 		c.mutex[i].RUnlock()
 	}
 	return keys
-}
-
-func (c *Counter) decr(key string, step int64) {
-	idx := c.hash(key)
-	c.mutex[idx].RLock()
-	defer c.mutex[idx].RUnlock()
-	if _, ok := c.pool[idx][key]; ok {
-		atomic.AddInt64(c.pool[idx][key].InternalCount, -step)
-	}
 }
